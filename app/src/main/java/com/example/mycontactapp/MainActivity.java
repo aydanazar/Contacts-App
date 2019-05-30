0c54916d48a37a73dc0a8710beb7fb4bbebb23ed
Ayda <ayda.nayebnazar@yahoo.ca>
2019-05-30 13:29:50 -0700

    finished contact app

---
+++ app/src/main/java/com/example/mycontactapp/MainActivity.java
@@ -0,0 +1,104 @@
+package com.example.mycontactapp;
+
+import android.database.Cursor;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    DatabaseHelper myDb;
+    EditText editName, editPhone, editAddress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("MyContactApp", "MainActivity: setting up layout");
+        setContentView(R.layout.activity_main);
+
+        editName = findViewById(R.id.editText_name);
+        editPhone = findViewById(R.id.editText_phone);
+        editAddress = findViewById(R.id.editText_address);
+
+        myDb = new DatabaseHelper(this);
+        Log.d("MyContactApp", "MainActivity: instantiated DatabaseHelper");
+    }
+
+    public void addData(View view){
+        Log.d("MyContactApp", "MainActivity: add data");
+        boolean isInserted = myDb.insertData(editName.getText().toString(), editPhone.getText().toString(), editAddress.getText().toString());
+
+        if(isInserted == true){
+            Toast.makeText(MainActivity.this, "Success - contact inserted", Toast.LENGTH_LONG).show();
+        }
+        else{
+            Toast.makeText(MainActivity.this, "Failed - contact not inserted", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    public void viewData(View view){
+        Log.d("MyContactApp", "MainActivity: view data");
+        Cursor res = myDb.getAllData();
+
+        if (res.getCount() == 0){
+            showMessage("Error", "No data found in the database");
+            return;
+        }
+
+        StringBuffer buffer = new StringBuffer();
+        while(res.moveToNext()){
+            //append res column 0, ... to the buffer - see Stringbuffer and Cursor api's
+            buffer.append("ID: " + res.getString(0) + "\n");
+            buffer.append("Name: " + res.getString(1) + "\n");
+            buffer.append("Phone: " + res.getString(2) + "\n");
+            buffer.append("Address: " + res.getString(3) + "\n");
+            buffer.append("\n");
+        }
+
+        showMessage("Data", buffer.toString());
+    }
+
+    public void showMessage(String title, String message){
+        //put Log.d's in here
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
+    }
+
+    public void searchData(View view){
+        Log.d("MyContactApp", "MainActivity: search data");
+        Cursor res = myDb.getAllData();
+
+        if (res.getCount()==0) {
+            showMessage("Error", "No data found in database");
+            return;
+        }
+
+        StringBuffer buffer = new StringBuffer();
+        while (res.moveToNext()) {
+            //append res column 0, ... to the buffer = see Stringbuffer and Cursor api's
+            boolean match = true;
+            if (editName.getText().toString().length()!=0 && !editName.getText().toString().equals(res.getString(1))) match=false;
+            if (editAddress.getText().toString().length()!=0 && !editAddress.getText().toString().equals(res.getString(2))) match=false;
+            if (editPhone.getText().toString().length()!=0 && !editPhone.getText().toString().equals(res.getString(3))) match=false;
+
+            if (match){
+                buffer.append("ID: " + res.getString(0) + "\n");
+                buffer.append("Name: " + res.getString(1) + "\n");
+                buffer.append("Number: " + res.getString(3) + "\n");
+                buffer.append("Address: " + res.getString(2) + "\n");
+                buffer.append("\n");
+            }
+        }
+
+        showMessage("Data", buffer.toString());
+    }
+
+}
